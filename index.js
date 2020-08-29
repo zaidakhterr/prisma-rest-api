9c45940766fcc26d20fb3bccb84977939d7bc3ee
Zaid Akhter <zaidakhter1202@gmail.com>
2020-08-29 21:53:26 +0500

    📦 NEW: Setup express server

--- index.js
+++ index.js
@@ -1,18 +1,27 @@
+const express = require("express");
 const { PrismaClient } = require("@prisma/client");
+const port = 4000;
+
+const app = express();
 
 const prisma = new PrismaClient({
   log: ["query", "info", "warn"],
 });
 
-async function main() {
-  const allAuthors = await prisma.author.findMany();
-  console.log(allAuthors);
-}
+// Middleware
+app.use(express.json());
 
-main()
-  .catch((e) => {
-    throw e;
-  })
-  .finally(async () => {
-    await prisma.$disconnect();
+// Routes
+app.get("/", async (req, res) => {
+  const allAuthors = prisma.author.findMany();
+  const allBooks = prisma.book.findMany();
+  return res.json({
+    authors: allAuthors,
+    books: allBooks,
   });
+});
+
+// Listen
+app.listen(port, () => {
+  console.log(`App started at http://localhost:${port}`);
+});
