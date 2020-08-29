b62e34f5eab643f5dcebd70416829aaeae69307d
Zaid Akhter <zaidakhter1202@gmail.com>
2020-08-29 23:08:05 +0500

    📦 NEW: POST /author

--- routes/author.js
+++ routes/author.js
@@ -1,7 +1,26 @@
 const router = require("express").Router();
+const prisma = require("../client");
 
-router.get("/author", (req, res) => {
-  return res.json("author");
+// Add Author
+router.post("/author", async (req, res) => {
+  const { name, email } = req.body;
+
+  try {
+    const newAuthor = await prisma.author.create({
+      data: {
+        email,
+        name,
+      },
+    });
+
+    res.json(newAuthor);
+  } catch (e) {
+    if (e.code === "P2002" && e.meta.target.includes("email")) {
+      res.status(400).json("Duplicate Email");
+    } else {
+      res.status(500).json(e);
+    }
+  }
 });
 
 module.exports = router;
