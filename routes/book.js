59666c008a0025d80c331e2c9aa65442fc90537a
Zaid Akhter <zaidakhter1202@gmail.com>
2020-08-30 05:15:50 +0500

    📦 NEW: POST /book

--- routes/book.js
+++ routes/book.js
@@ -1,4 +1,41 @@
 const router = require("express").Router();
 const prisma = require("../client");
 
+// Add Book
+router.post("/book", async (req, res) => {
+  const { title, authorId } = req.body;
+
+  try {
+    const author = await prisma.author.findOne({
+      where: {
+        id: Number(authorId),
+      },
+    });
+
+    if (!author) {
+      return res.status(404).json("Author does not exist");
+    }
+
+    const newBook = await prisma.book.create({
+      data: {
+        title,
+        author: {
+          connect: {
+            id: Number(authorId),
+          },
+        },
+      },
+      select: {
+        id: true,
+        title: true,
+        author: true,
+      },
+    });
+
+    return res.json(newBook);
+  } catch (e) {
+    return res.status(500).json(e);
+  }
+});
+
 module.exports = router;
